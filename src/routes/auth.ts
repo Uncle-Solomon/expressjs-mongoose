cd3e60311ffb9a4aa1c0402846fcb60e15652a0c
Ameh Solomon Onyeke <amehsolomon46@gmail.com>
2023-09-01 11:33:19 +0100

    test

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -2,7 +2,9 @@ import express from "express";
 import { signup } from "../controllers/auth/signup";
 import { login } from "../controllers/auth/login";
 
-export const authRoute = express.Router();
+const authRoute = express.Router();
 
 authRoute.post("/signup", signup);
 authRoute.post("/login", login);
+
+export default authRoute;
